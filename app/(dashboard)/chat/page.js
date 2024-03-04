d293412df1f3e32ffdc999acdca2eb069cb13569
Arian <Arianbod@outlook.com>
2024-03-04 17:33:51 +0300

    pages created

---
+++ app/(dashboard)/chat/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const chatPage = () => {
+    return (
+        <div>chatPage</div>
+    )
+}
+
+export default chatPage
---
+++ app/(dashboard)/tours/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const toursPage = () => {
+    return (
+        <div>toursPage</div>
+    )
+}
+
+export default toursPage

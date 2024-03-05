031e66db291159c461393896b9e44f5ed01d9f5c
Arian <Arianbod@outlook.com>
2024-03-05 21:55:15 +0300

    created

---
+++ components/chat/Chat.jsx
@@ -0,0 +1,89 @@
+'use client';
+import { generateChatResponse } from '@/utils/action';
+import { useMutation } from '@tanstack/react-query';
+import { nanoid } from 'nanoid';
+import React, { useEffect, useRef, useState } from 'react';
+import toast from 'react-hot-toast';
+import { IoMdSend } from 'react-icons/io';
+import { FaUserLarge } from 'react-icons/fa6';
+import { BsChatRightText } from 'react-icons/bs';
+const Chat = () => {
+	const [text, setText] = useState('');
+	const [messages, setMessages] = useState([]);
+	const messagesEndRef = useRef(null); // Ref for the container's bottom
+	const { mutate, isPending } = useMutation({
+		mutationFn: (query) => generateChatResponse([...messages, query]),
+		onSuccess: (data) => {
+			if (!data) {
+				toast.error('there is no data');
+				return;
+			}
+			setMessages((prev) => [...prev, data]);
+		},
+	});
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const query = { role: 'user', content: text };
+		mutate(query);
+		setMessages((prev) => [...prev, query]);
+		setText('');
+	};
+	useEffect(() => {
+		// Scroll to the bottom of the messages container
+		messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+	}, [messages]); // Dependency array ensures effect runs when messages change
+
+	return (
+		<div className='min-h-[calc(100vh-6rem)] grid grid-rows-[1fr,auto] -mx-8'>
+			<div className='max-h-[calc(100vh-12rem)] overflow-y-auto overflow-x-hidden'>
+				<ul>
+					{messages.map(({ role, content }, index) => {
+						const id = nanoid();
+						return (
+							<li
+								key={id}
+								className={` flex items-baseline gap-3 py-6 px-4 text-lg leading-loose border-b border-base-300 ${
+									role === 'assistant' && 'bg-base-100  px-12'
+								}`}>
+								<span className='avatar'>
+									{role === 'user' && <FaUserLarge />}
+									{role === 'assistant' && <BsChatRightText />}
+								</span>
+								<p className='max-w-3xl'>{content}</p>
+								{isPending && messages.length === index + 1 && (
+									<span className='loading'></span>
+								)}
+							</li>
+						);
+					})}
+					<div ref={messagesEndRef} />
+					{/* Invisible element at the end of the messages */}
+				</ul>
+			</div>
+			<form
+				onSubmit={handleSubmit}
+				className='max-w-4xl px-8 pt-12'>
+				<div className='join w-full'>
+					<input
+						type='text'
+						placeholder='Message GeniusGPT'
+						className='input input-bordered join-item w-full'
+						value={text}
+						onChange={(e) => {
+							setText(e.target.value);
+						}}
+					/>
+					<button
+						className='btn btn-primary join-item'
+						type='submit'
+						disabled={isPending}>
+						{isPending ? 'wait...' : <IoMdSend />}
+					</button>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default Chat;

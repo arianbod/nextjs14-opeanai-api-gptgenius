404879d56c92d5efab605bde5964d02991bedb6b
Arian <Arianbod@outlook.com>
2024-03-06 01:30:24 +0300

    created

---
+++ components/tours/ToursList.jsx
@@ -0,0 +1,21 @@
+'use client';
+import React from 'react';
+import TourCard from './TourCard';
+const ToursList = ({ data }) => {
+	if (data.length === 0) return <h4 className='text-lg'> no tours found...</h4>;
+
+	return (
+		<div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-8'>
+			{data.map((tour) => {
+				return (
+					<TourCard
+						{...tour}
+						key={tour.id}
+					/>
+				);
+			})}
+		</div>
+	);
+};
+
+export default ToursList;

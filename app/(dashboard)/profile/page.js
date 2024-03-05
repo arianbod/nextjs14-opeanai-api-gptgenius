a9341e56af0d8e09a03427ea14ea4327d775f68d
Arian <Arianbod@outlook.com>
2024-03-06 01:29:09 +0300

    tokens added

--- app/(dashboard)/profile/page.js
+++ app/(dashboard)/profile/page.js
@@ -1,9 +1,16 @@
-import { UserProfile } from '@clerk/nextjs'
+import { fetchUserTokensById } from '@/utils/action'
+import { UserProfile, auth } from '@clerk/nextjs'
 import React from 'react'
 
-const profilePage = () => {
+const profilePage = async () => {
+    const { userId } = auth()
+    const currentTokens = await fetchUserTokensById(userId)
     return (
-        <div><UserProfile /></div>
+        <div>
+            <h2 className='mb-8 ml-8 text-xl font-extrabold'>
+                Token Amount:{currentTokens}
+            </h2>
+            <UserProfile /></div>
     )
 }
 

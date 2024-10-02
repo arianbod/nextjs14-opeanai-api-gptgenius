cf4ffcb9fc1fc255cf9dc4964b6841a0c27ebc84
Arian <Arianbod@outlook.com>
2024-10-02 20:40:47 +0300

    0.0.9.0.2 small but major update in auth mechanism done

--- components/sidebar/MemberProfile.jsx
+++ components/sidebar/MemberProfile.jsx
@@ -1,24 +1,23 @@
+// components/MemberProfile.jsx
 'use client';
 import React from 'react';
-import { useRouter } from 'next/navigation';
+import { useAuth } from '@/context/AuthContext';
 import { FiLogOut } from 'react-icons/fi';
 import Loading from '../Loading';
 
-const MemberProfile = ({ user }) => {
-	const router = useRouter();
+const MemberProfile = () => {
+	const { user, logout } = useAuth();
 
 	if (!user) {
 		return <Loading />;
 	}
 
 	const handleSignOut = () => {
-		localStorage.removeItem('user');
-		document.cookie = 'user=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-		router.push('/');
+		logout();
 	};
 
 	return (
-		<div className='px-4 flex border-t-2 pt-4 justify-between  w-full items-center gap-2 mr-auto'>
+		<div className='px-4 flex border-t-2 pt-4 justify-between w-full items-center gap-2 mr-auto'>
 			<p>{user.token.substring(0, 8)}...</p>
 			<button
 				onClick={handleSignOut}

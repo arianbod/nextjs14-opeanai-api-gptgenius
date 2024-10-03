07913bda5f08c98b4c35e03f54249929dee08135
Arian <Arianbod@outlook.com>
2024-10-04 01:11:32 +0300

    0.0.9.0.4 safe small tweaks done

--- app/providers.js
+++ app/providers.js
@@ -1,7 +1,6 @@
 'use client'
 
 import { AuthProvider } from '@/context/AuthContext'
-import { ChatProvider } from '@/context/ChatContext'
 import { MessageProvider } from '@/context/MessageContext'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
@@ -22,10 +21,7 @@ const Providers = ({ children }) => {
         <QueryClientProvider client={queryClient}>
             <MessageProvider>
                 <AuthProvider>
-                    <ChatProvider>
-
-                        {children}
-                    </ChatProvider>
+                    {children}
                 </AuthProvider>
             </MessageProvider>
         </QueryClientProvider>

59f62954a287926e99b7cb3005b35e3f86178c13
Arian <Arianbod@outlook.com>
2024-10-04 18:25:08 +0300

    0.0.9.1.6 change page name and component name done

--- app/(dashboard)/chat/[...chatId]/page.js
+++ app/(dashboard)/chat/[...chatId]/page.js
@@ -1,14 +1,14 @@
 "use client"
-import EnhancedChat from '@/components/chat/ChatPage'
+import ChatPage from '@/components/chat/ChatPage'
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
 
-const ChatRoom = ({ params }) => {
+const page = ({ params }) => {
     const queryClient = new QueryClient()
     return (
         <HydrationBoundary state={dehydrate(queryClient)}>
-            <EnhancedChat chatId={params.chatId} />
+            <ChatPage chatId={params.chatId} />
         </HydrationBoundary>
     )
 }
 
-export default ChatRoom
+export default page

7781fd4f3d45bd9876782b94e092bce4426c1666
Arian <Arianbod@outlook.com>
2024-12-07 21:23:28 +0300

    make loading center even on lage screens 1.4  build 0.5.3.6 - stable

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loading = () => {
 	return (
-		<div className='flex place-content-center place-items-center h-screen w-full z-40 fixed '>
+		<div className='flex place-content-center place-items-center h-screen w-full left-0 right-0 m-auto top-0 bottom-0 z-40 fixed '>
 			<div className='rounded-full w-12 h-12 border-b-2 border-blue-500 animate-spin'></div>
 		</div>
 	);
